9db95ce0071812addb68dde74fa210118f9b9dc9
Karolina Redden <dubajkaro@gmail.com>
2023-04-03 12:35:11 -0300

    add phone number

---
+++ need-help.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="main.css" type="text/css" />
+  <title>Need help?</title>
+</head>
+<body>
+
+<?php include "header.php"; ?>
+<div margin=300px>
+<a href=“tel:555-666-7777”>Call: 555-666-7777</a>
+</div>
+
+
+
+<?php include "footer.php"; ?>
+
+</body>
+</html>
