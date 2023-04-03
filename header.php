114dc74ba8b16b199a1ffb6c4f228db3259e4b50
Karolina Redden <dubajkaro@gmail.com>
2023-04-03 10:43:49 -0300

    add header template

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Header</title>
+</head>
+<body>
+<header id="header_top">
+      <nav>
+        <ul>
+          <li><img id="hamburger" src="images/hamburger.png" /></li>
+          <li id="header_text">Halifax Canoe and Kayak</li>
+          <li><img src="images/paddle-white.png" alt="Paddle logo" /></li>
+        </ul>
+      </nav>
+    </header>
+  
+</body>
+</html>
