166e9cb03af2f36d8cc431afb76f10e18f182649
Karolina Redden <dubajkaro@gmail.com>
2023-04-03 11:55:27 -0300

    include files

--- header.php
+++ header.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Header</title>
-</head>
-<body>
+<?php
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+// if ( !isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true ) {
+//     header("location: login.php");
+//     exit;
+// }
+?>
+
+
 <header id="header_top">
       <nav>
         <ul>
@@ -15,6 +17,4 @@
           <li><img src="images/paddle-white.png" alt="Paddle logo" /></li>
         </ul>
       </nav>
-    </header>
-</body>
-</html>
+</header>
