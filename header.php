50fb3ed6378acb11a393ce0b16f619a50cd1a7c1
Karolina Redden <dubajkaro@gmail.com>
2023-04-03 20:12:48 -0300

    add session to header

--- header.php
+++ header.php
@@ -1,17 +1,23 @@
 <?php
 session_start();
-// Check if the user is logged in, if not then redirect him to login page
-// if ( !isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true ) {
-//     header("location: login.php");
-//     exit;
+// $_SESSION["name"] = "Karolina";
+ 
+// // Check if the user is logged in, if not then redirect him to login page
+// // if ( !isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true ) {
+// //     header("location: login.php");
+// //     exit;
 // }
 ?>
 <header id="header_top">
-      <nav>
-        <ul>
-          <li><img id="hamburger" src="images/hamburger.png" /></li>
-          <li id="header_text">Halifax Canoe and Kayak</li>
-          <li><img src="images/paddle-white.png" alt="Paddle logo" /></li>
-        </ul>
-      </nav>
+  <nav>
+    <ul>
+      <li><img id="hamburger" src="images/hamburger.png" /></li>
+      <li id="header_text">Halifax Canoe and Kayak</li>
+      <li><img src="images/paddle-white.png" alt="Paddle logo" /></li>
+    </ul>
+  </nav>
 </header>
+
+<div>
+ 
+</div>
