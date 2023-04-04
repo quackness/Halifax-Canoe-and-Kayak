1058d6647f614bbf877e2215eb2a2b983c18b88d
Karolina Redden <dubajkaro@gmail.com>
2023-04-04 15:56:46 -0300

    style logout page

--- logout.php
+++ logout.php
@@ -1,15 +1,6 @@
-<?php 
-session_start();
-$_SESSION['username'] = 'Karolina';
-echo "\n";
-$_SESSION = [];
-session_destroy();
-echo isset($_SESSION['username']) ? $_SESSION['username'] : 'Session destroyed';
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,16 +8,31 @@ echo isset($_SESSION['username']) ? $_SESSION['username'] : 'Session destroyed';
   <link rel="stylesheet" href="main.css" type="text/css" />
   <title>Logout</title>
 </head>
+
 <body>
-  <!-- <?php include "header.php"; ?> -->
-
-
-  <div class="logout-p">
-    <p>You have been logged out</p>
-  </div>
+  <?php include "header.php"; ?>
+  <div style="
+        width: 500px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 140px;
+      ">
     
+
+    <?php
+    $_SESSION['username'] = 'Karolina';
+    echo "\n";
+    $_SESSION = [];
+    session_destroy();
+    echo isset($_SESSION['username']) ? $_SESSION['username'] : 'Session destroyed';
+    printf("<br><h3>You have been logged out.</h3>
+<br><h3><a href=index.php>Return to the home page 🔙 </a></h3>")
+?>
+     
+</div>
+
   <?php include "footer.php"; ?>
+ 
 </body>
+
 </html>
-
-
