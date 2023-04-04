25fb30867353c97a32322d6a65e5b3068f044ea7
Karolina Redden <dubajkaro@gmail.com>
2023-04-03 21:24:32 -0300

    style the page

--- isnt-working.php
+++ isnt-working.php
@@ -1,18 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="main.css" type="text/css" />
-  <title>Isn't working?</title> 
+  <title>Isn't working?</title>
 </head>
+
 <body>
   <?php include "header.php"; ?>
-  <div class="gify">
-    <iframe src="https://giphy.com/embed/4IwnscWi8r5NRuLGRW" width="400" height="480">
-    </iframe>
+  <div style="
+        width: 500px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 140px;
+      ">
+<h3>So your computer isn't working?</h3>
+      <iframe src="https://giphy.com/embed/4IwnscWi8r5NRuLGRW" width="500" height="480">
+      </iframe>
+  
   </div>
   <?php include "footer.php"; ?>
 </body>
-</html> 
+
+</html>
