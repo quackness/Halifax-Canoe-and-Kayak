37b1d391705bbaef6c58f7b64160926e9f82ee6f
Karolina Redden <dubajkaro@gmail.com>
2023-04-03 22:00:35 -0300

    add new accoutn form

--- new-account.php
+++ new-account.php
@@ -4,10 +4,30 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="main.css" type="text/css" />
   <title>New account</title>
 </head>
 <body>
-  <p>Test</p>
+<?php include "header.php"; ?>
+  <div style="
+        width: 500px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 140px;
+      ">
+      <form method="post" action="send-email.php">
+        <label for="customeremail">Your email:</label>
+        <input type="email" name="customeremail" id="c">
+
+        <label for="message">Your message:</label>
+        <textarea name="message" id="message"></textarea>
+
+        <input type="submit" name="submit" value="Send message">
+      </form>
+
+
+    </div>
+<?php include "footer.php"; ?>
   
 </body>
 </html>
