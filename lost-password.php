38dc387f70268bd06d295cec2f60f7f794d9f97f
Karolina Redden <dubajkaro@gmail.com>
2023-04-04 22:19:39 -0300

    change form style

--- lost-password.php
+++ lost-password.php
@@ -6,6 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="main.css" type="text/css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <title>Lost password</title>
 </head>
 
@@ -19,14 +20,15 @@
       ">
      <?php include "buttonToLogout.php"; ?>
     <form method="get" action="send-email.php">
+    <div class="mb-3">
       <label for="email">Your email:</label>
-      <input type="email" name="email">
-
+      <input type="email" name="email" class="form-control">
+    </div>
+    <div class="mb-3">
       <label for="message">Your message:</label>
-      <textarea name="message" id="message"></textarea>
-
+      <textarea name="message" id="message" class="form-control"></textarea>
+    </div>
       <input type="hidden" name="lostPassword" value="lostPassword">
-
       <input type="submit" name="submit" value="Send message">
     </form>
   </div>
