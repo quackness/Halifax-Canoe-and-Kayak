71ef2cf2e2055a99b68a78487327a5ed806b34c8
Karolina Redden <dubajkaro@gmail.com>
2023-04-05 22:21:44 -0300

    add else when the log out did not remove a cookie session

--- logout.php
+++ logout.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="main.css" type="text/css" />
   <title>Logout</title>
 </head>
-<body>
+<body style="background-color: #dbdcdd">
   <?php include "header.php"; ?>
   <div style="
         width: 500px;
@@ -18,13 +18,18 @@
 
 
     <?php
-    $_SESSION['username'] = 'Karolina';
-    echo "\n";
+    // $_SESSION['username'] = 'Karolina';
+    // echo "\n";
     $_SESSION = [];
     session_destroy();
-    echo isset($_SESSION['username']) ? $_SESSION['username'] : 'Session destroyed';
-    printf("<br><h3>You have been logged out.</h3>
-<br><h3><a href=index.php>Return to the home page 🔙 </a></h3>")
+    // echo isset($_SESSION['username']) ? $_SESSION['username'] : 'Session destroyed';
+    if (!$_SESSION) {
+      printf("<br><h3>You have been logged out.</h3>
+      <br><h3><a href=index.php>Return to the home page 🔙 </a></h3>");
+    } else {
+      printf("<br><h3>Logging out not successful.</h3>
+      <br><h3><a href=index.php>Return to the home page and create a support ticket🔙 </a></h3>");
+    }
       ?>
   </div>
   <?php include "footer.php"; ?>
