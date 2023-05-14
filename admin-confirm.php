62305ba9ae8ee2e02a2ddca704691cb0df892216
Karolina Redden <dubajkaro@gmail.com>
2023-05-14 20:52:12 -0300

    add confirmation page

---
+++ admin-confirm.php
@@ -0,0 +1,22 @@
+<body>
+  <?php include "header.php"; ?>
+  <div style="
+        width: 500px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 250px;
+      ">
+
+  </div>
+  <div>
+    <h1>Admin - Confirm</h1>
+    <hr>
+    </hr>
+    <h3>Date has been added successfully to a database.</h3>
+    <h2><a href="/assignments/kayak_club/all-adventures.php">View All Adventures</h2>
+  </div>
+
+  <?php include "footer.php"; ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
